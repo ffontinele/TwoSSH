f8b2ff67d67269734892d4470ea397eb41b5a815
twossh <38133710+twossh@users.noreply.github.com>
2018-07-06 05:16:45 -0300

    Update socksauto.sh

--- socksauto.sh
+++ socksauto.sh
@@ -4,8 +4,8 @@ NUMBER=$(ps -x | grep -c "python")
 if [ $NUMBER = "3" ]; then
     echo "" > /dev/null
 else
-    nohup python /bin/proxy80.py > proxy80.txt &
-    nohup python /bin/proxy433.py > proxy443.txt &
+    nohup python /bin/proxy80.py > /bin/proxy80.txt &
+    nohup python /bin/proxy433.py > /bin/proxy443.txt &
 fi
    
    
